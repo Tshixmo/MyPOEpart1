6952354cc0d98986fb7bd7469b9847e59de7e7e1
Omphemetse Mokoena <ST10291971@imconnect.edu.za>
2024-04-10 19:20:52 +0200

    Created all the methods needed for the application

--- Coding/Program.cs
+++ Coding/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyRecipe
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
+
+class Recipe
+{
+    private string[] ingredients;
+    private double[] quantity;
+    private string[] units;
+    private string[] steps;
+
+    public void TheRecipeDetails()
+    {
+
+    }
+
+    public void DisplayingTheRecipe()
+    {
+
+    }
+
+    public void Scaling()
+    {
+
+    }
+
+    public void Reset()
+    {
+
+    }
+
+    public void ClearData()
+    {
+        ingredients = null;
+        quantity = null;
+        units = null;
+        steps = null;
+        Console.WriteLine("Data Cleared");
+    }
+
+}
