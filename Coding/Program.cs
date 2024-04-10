a415f72ff72f49e3dac85218e726fd6a2527515c
Omphemetse Mokoena <ST10291971@imconnect.edu.za>
2024-04-10 19:25:49 +0200

    Coded the First method which will as the user for the recipe details.

--- Coding/Program.cs
+++ Coding/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers.Text;
 
 namespace MyRecipe
 {
@@ -20,6 +21,39 @@ class Recipe
 
     public void TheRecipeDetails()
     {
+        //Ask the number of ingredients the user wants to enter
+        Console.WriteLine("Enter the number of ingredients your recipe will have? ");
+        int numIngred = Convert.ToInt32(Console.ReadLine());
+
+        //Creating arrays for the ingredient, quantity and units to be stored in. With the number of ingredients
+        //as the 
+        ingredients = new string[numIngred];
+        quantity = new double[numIngred];
+        units = new string[numIngred];
+
+        // This will iterate based on the number of ingredient they are. 
+        for (int i = 0; i < numIngred; i++)
+        {
+            Console.WriteLine("Enter the name of the ingredient: ");
+            ingredients[i] = Console.ReadLine();
+
+            Console.WriteLine("Enter the quantity for the " + ingredients[i] + " :");
+            quantity[i] = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the unit of measurement for the " + ingredients[i] + " :");
+            units[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Enter the number of steps the recipe has");
+        int stepNum = Convert.ToInt32(Console.ReadLine());
+
+        steps = new string[stepNum];
+
+        for (int i = 0; i < stepNum; i++)
+        {
+            Console.WriteLine("Enter step" + (i + 1) + ":");
+            steps[i] = Console.ReadLine();
+        }
 
     }
 
