3a61d7ffd6f0d7e82230b6bc0ec2cd21f2b18703
Omphemetse Mokoena <ST10291971@imconnect.edu.za>
2024-04-10 21:44:23 +0200

    Added some spacing between outputs and made the outputs look better.

--- Coding/Program.cs
+++ Coding/Program.cs
@@ -49,99 +49,104 @@ namespace MyRecipe
                         Console.WriteLine("Invalid option. Please try again.");
                         break;
                 }
+            }
         }
     }
-}
 
-class Recipe
-{
-    private string[] ingredients;
-    private double[] quantity;
-    private string[] units;
-    private string[] steps;
-
-    public void TheRecipeDetails()
+    class Recipe
     {
-        //Ask the number of ingredients the user wants to enter
-        Console.WriteLine("Enter the number of ingredients your recipe will have? ");
-        int numIngred = Convert.ToInt32(Console.ReadLine());
+        private string[] ingredients;
+        private double[] quantity;
+        private string[] units;
+        private string[] steps;
 
-        //Creating arrays for the ingredient, quantity and units to be stored in. With the number of ingredients
-        //as the 
-        ingredients = new string[numIngred];
-        quantity = new double[numIngred];
-        units = new string[numIngred];
-
-        // This will iterate based on the number of ingredient they are. 
-        for (int i = 0; i < numIngred; i++)
+        public void TheRecipeDetails()
         {
-            Console.WriteLine("Enter the name of the ingredient: ");
-            ingredients[i] = Console.ReadLine();
+            //Ask the number of ingredients the user wants to enter
+            Console.WriteLine("Enter the number of ingredients your recipe will have? ");
+            int numIngred = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the quantity for the " + ingredients[i] + " :");
-            quantity[i] = Convert.ToDouble(Console.ReadLine());
+            //Creating arrays for the ingredient, quantity and units to be stored in. With the number of ingredients
+            //as the 
+            ingredients = new string[numIngred];
+            quantity = new double[numIngred];
+            units = new string[numIngred];
 
-            Console.WriteLine("Enter the unit of measurement for the " + ingredients[i] + " :");
-            units[i] = Console.ReadLine();
+            // This will iterate based on the number of ingredient they are. 
+            for (int i = 0; i < numIngred; i++)
+            {
+                Console.WriteLine("Enter the name of the ingredient: ");
+                ingredients[i] = Console.ReadLine();
+
+                Console.WriteLine("Enter the quantity for the " + ingredients[i] + " :");
+                quantity[i] = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter the unit of measurement for the " + ingredients[i] + " :");
+                units[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter the number of steps the recipe has");
+            int stepNum = Convert.ToInt32(Console.ReadLine());
+
+            steps = new string[stepNum];
+
+            for (int i = 0; i < stepNum; i++)
+            {
+                Console.WriteLine("Enter step" + (i + 1) + ":");
+                steps[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
         }
 
-        Console.WriteLine("Enter the number of steps the recipe has");
-        int stepNum = Convert.ToInt32(Console.ReadLine());
-
-        steps = new string[stepNum];
-
-        for (int i = 0; i < stepNum; i++)
+        public void DisplayingTheRecipe()
         {
-            Console.WriteLine("Enter step" + (i + 1) + ":");
-            steps[i] = Console.ReadLine();
+            //Displaying the recipe and the ingredient in it. 
+            Console.WriteLine("Ingredient: ");
+            Console.WriteLine("Recipe: ");
+
+            for (int i = 0; i < ingredients.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + quantity[i] + units[i] + " " + "of " + ingredients[i]);
+            }
+
+            //Displaying the steps in the recipe.
+            Console.WriteLine("Steps: ");
+            for (int i = 0; i < steps.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + steps[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public void Scaling()
+        {
+            Console.WriteLine("Enter the scaling factor for your recipe: 0.5, 2 or 3");
+            double factor = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 0; i < quantity.Length; i++)
+            {
+                quantity[i] *= factor;
+            }
+
+            Console.WriteLine("Your Recipe has been successfully scaled by " + factor);
+            Console.WriteLine();
+        }
+
+        public void Reset()
+        {
+            Console.WriteLine("Quantity reset to orignal values");
+            Console.WriteLine();
+        }
+
+        public void ClearData()
+        {
+            ingredients = null;
+            quantity = null;
+            units = null;
+            steps = null;
+            Console.WriteLine("Data Cleared");
+            Console.WriteLine();
         }
 
     }
-
-    public void DisplayingTheRecipe()
-    {
-        //Displaying the recipe and the ingredient in it. 
-        Console.WriteLine("Ingredient: ");
-        Console.WriteLine("Recipe: ");
-
-        for (int i = 0; i < ingredients.Length; i++)
-        {
-            Console.WriteLine((i + 1) + "." + quantity[i] + units[i] + "of" + ingredients[i]);
-        }
-
-        //Displaying the steps in the recipe.
-        Console.WriteLine("Steps: ");
-        for (int i = 0; i < steps.Length; i++)
-        {
-            Console.WriteLine((i + 1) + "." + steps[i]);
-        }
-    }
-
-    public void Scaling()
-    {
-        Console.WriteLine("Enter the scaling factor for your recipe: 0.5, 2 or 3");
-        double factor = Convert.ToDouble(Console.ReadLine());
-
-        for (int i = 0; i < quantity.Length; i++)
-        {
-            quantity[i] *= factor;
-        }
-
-        Console.WriteLine("Your Recipe has been successfully scaled by " + factor);
-    }
-
-    public void Reset()
-    {
-        Console.WriteLine("Quantity reset to orignal values");
-    }
-
-    public void ClearData()
-    {
-        ingredients = null;
-        quantity = null;
-        units = null;
-        steps = null;
-        Console.WriteLine("Data Cleared");
-    }
-
 }
